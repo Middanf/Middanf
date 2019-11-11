c8e4470faf7c7cfa3be25f067d19fdfe817c95c6
thedavos <david@davosdo.com>
2019-11-10 23:47:39 -0500

    conectando la base de datos a google cloud platform

--- src/utils/MysqlDBConexion.java
+++ src/utils/MysqlDBConexion.java
@@ -8,9 +8,17 @@ public class MysqlDBConexion {
 		Connection cn=null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String connectionURL = "jdbc:mysql://localhost:3306/lms?"
-					+ "user=root&password=mysql";
-			cn=DriverManager.getConnection(connectionURL);
+			String databaseName = "middanf_database";
+			String username = "root";
+			String password = "qhkbgCp9t*FkgwV_@gvK";
+			String instanceConnectionName = "middanf:us-east1:middanf-db";
+			String jdbcUrl = String.format(
+				    "jdbc:mysql://google/%s?cloudSqlInstance=%s"
+				        + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+				    databaseName,
+				    instanceConnectionName);
+			
+			cn=DriverManager.getConnection(jdbcUrl, username, password);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
